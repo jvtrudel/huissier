8e919f91802b04e57b923265a0070191fbf8b47d
jvtrudel <jvtrudel@gmx.com>
2017-04-16 17:56:38 -0400

    caractères spéciaux
    modifie pour pouvoir copier les fichiers contenant  des caractères spéciaux

--- scripts/copy-doc.sh
+++ scripts/copy-doc.sh
@@ -2,10 +2,7 @@
 
 # À partir de $1, un fichier contenant une liste de fichiers, copier les fhichiers dans le répertoire $2
 
-
-paths=(`cat $1`)
-
-for path in  "${paths[@]}"
+while read path;
   do
-    cp ${path} $2/.
-done
+    cp "${path}" $2/.
+done <$1
